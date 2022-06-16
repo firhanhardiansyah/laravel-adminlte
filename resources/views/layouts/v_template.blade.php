f4722fba24f728632bb2684eed8ce0bf81e7c68e
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2022-06-16 11:36:30 +0700

    Initial Commit

---
+++ resources/views/layouts/v_template.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }} | @yield('title')</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('template') }}/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('template') }}/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- Navbar -->
+        @include('layouts.v_navbar')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('layouts.v_sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>@yield('title')</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <?php $link = ''; ?>
+                                @for ($i = 1; $i <= count(Request::segments()); $i++)
+                                    <li class="breadcrumb-item">
+                                        @if (($i < count(Request::segments())) & ($i > 0))
+                                            <?php $link .= '/' . Request::segment($i); ?>
+                                            <a href="<?= $link ?>">
+                                                {{ ucwords(str_replace('-', ' ', Request::segment($i))) }}
+                                            </a>
+                                        @else
+                                            {{ ucwords(str_replace('-', ' ', Request::segment($i))) }}
+                                        @endif
+                                    </li>
+                                @endfor
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+                @yield('content')
+
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        @include('layouts.v_footer')
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('template') }}/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('template') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('template') }}/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('template') }}/dist/js/demo.js"></script>
+</body>
+
+</html>
---
+++ resources/views/v_about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.v_template')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>This is about</h1>
+@endsection
