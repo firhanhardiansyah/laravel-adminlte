98714f494a9f84d685bd12ade97a011c9c3a6bc1
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2022-06-16 12:22:39 +0700

    Remove AdminLTE for demo purposes

--- resources/views/layouts/v_template.blade.php
+++ resources/views/layouts/v_template.blade.php
@@ -81,8 +81,6 @@
     <script src="{{ asset('template') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('template') }}/dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('template') }}/dist/js/demo.js"></script>
 </body>
 
 </html>
